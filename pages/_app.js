8bb3aacc2cdadf3dfdf2f105cce037f203345b07
Alert175 <ilya290897@gmail.com>
2021-03-10 23:57:22 +0300

    backup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import '../styles/globals.css'
+import '../styles/globals.scss'
 
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -13,20 +13,20 @@ export default function MyApp({ Component, pageProps }) {
         />
         <meta name="description" content="Description" />
         <meta name="keywords" content="Keywords" />
-        <title>Next.js PWA Example</title>
+        <title>Current-Quotes</title>
 
         <link rel="manifest" href="/manifest.json" />
         <link
-          href="/icons/favicon-16x16.png"
+          href="/icons/1.png"
           rel="icon"
           type="image/png"
-          sizes="16x16"
+          sizes="48x48"
         />
         <link
-          href="/icons/favicon-32x32.png"
+          href="/icons/2.png"
           rel="icon"
           type="image/png"
-          sizes="32x32"
+          sizes="72x72"
         />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         <meta name="theme-color" content="#317EFB" />
